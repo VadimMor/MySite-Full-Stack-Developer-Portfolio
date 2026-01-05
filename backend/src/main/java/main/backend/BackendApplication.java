b05896680d7945aaa4bb316e70cbe20b3d24e6ef
VadimMor <rich246135@gmail.com>
2026-01-05 21:21:16 +0300

    Get methods by file

--- backend/src/main/java/main/backend/BackendApplication.java
+++ backend/src/main/java/main/backend/BackendApplication.java
@@ -2,8 +2,12 @@ package main.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "main.postgres.repository")
+//@EntityScan(basePackages = "main.postgres.entity")
 public class BackendApplication {
 
 	public static void main(String[] args) {

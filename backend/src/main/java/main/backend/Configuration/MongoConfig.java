ccaa56509d2f5cede57f202e59c3239155c63450
VadimMor <rich246135@gmail.com>
2026-01-14 10:21:24 +0300

    Adding docker and configuration mongo

--- backend/src/main/java/main/backend/Configuration/MongoConfig.java
+++ backend/src/main/java/main/backend/Configuration/MongoConfig.java
@@ -1,4 +1,36 @@
 package main.backend.Configuration;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
 public class MongoConfig {
+    // Сюда попадет твоя строка из Docker Compose (MONGODB_URL)
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionString;
+
+    // Сюда попадет имя базы данных (MONGODB_DB)
+    @Value("${MONGODB_DB}")
+    private String databaseName;
+
+    @Bean
+    public MongoClient mongoClient() {
+        ConnectionString connString = new ConnectionString(connectionString);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connString)
+                .build();
+        return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        // Создаем шаблон для работы с базой, используя наш клиент и имя базы
+        return new MongoTemplate(mongoClient(), databaseName);
+    }
 }
